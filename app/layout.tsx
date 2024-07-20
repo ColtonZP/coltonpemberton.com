63fa0795a394671c92305a8b238a69cffd82a7e0
colton <colton@coltonpemberton.com>
2024-07-20 00:28:05 -0700

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Analytics />
+      <SpeedInsights />
       <body>{children}</body>
     </html>
   );
